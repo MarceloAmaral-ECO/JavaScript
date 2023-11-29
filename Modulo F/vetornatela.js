a15e54bdf6389841d719ed826a12646bd3a3f29c
MarceloAmaral-ECO <105668469+MarceloAmaral-ECO@users.noreply.github.com>
2023-11-29 16:37:11 -0300

    end course

---
+++ Modulo F/vetornatela.js
@@ -0,0 +1,17 @@
+let valores = [8,1,7,4,2,9]
+
+//for (let i=0; i < valores.length; i++) {
+//    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+//}
+
+//for (let pos in valores) {
+//    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+//}
+
+let pos = valores.indexOf(3)
+
+if (pos == -1) {
+    console.log("O valor não foi encontrado")
+} else {
+    console.log(`O valor procurado está na posição ${pos}`)
+}
